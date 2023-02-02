00e1d9d9d118a041c54fcbfaa33340d66e7e92a6
nedu-ramzi <ramseychinedu@gmail.com>
2023-02-02 11:58:53 +0000

    a function that adds 2 integers.

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,7 +5,8 @@ This module has one function that adds up 2 integers
 
 
 def add_integer(a, b=98):
-    """Return the sum of two integers or floats as integers
+    """
+    Return the sum of two integers or floats as integers
     Args:
         a: first argument
         b: second argument
