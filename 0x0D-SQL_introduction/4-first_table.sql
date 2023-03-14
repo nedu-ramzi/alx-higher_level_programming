0da8c933935e57944a0f5394bcfc3fb4b5ec98a2
nedu-ramzi <ramseychinedu@gmail.com>
2023-03-14 21:18:31 +0000

    a table called first_table in the current database in your MySQL server.

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- a script that creates a table called first_table in the current database in your MySQL server.
+-- The database name will be passed as an argument of the mysql command
+
+CREATE TABLE first_table (id INT, name VARCHAR(256));
