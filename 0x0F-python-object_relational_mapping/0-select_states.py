5e7797bf9a2f2e228e3fccf034a097271ecbb93a
nedu-ramzi <ramseychinedu@gmail.com>
2023-04-17 17:59:37 +0000

    a script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-""Lists all the states from the database hbrn_0e_0_usa"""
+""
+    Lists all the states from the database hbrn_0e_0_usa
+"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(
+    db_connect = MySQLdb.connect(
         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states;")
-    states = cursor.fetchall()
+    db_cursor = db_connect.cursor()
 
-    for state in states:
-        print(state)
+    db_cursor.execute("SELECT * FROM states")
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
